5518200598ff612af0c291d34ef3a1c5fb8060cd
syeony <fltkfltk1004@naver.com>
2025-07-19 01:00:10 +0900

    .

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from utils import get_most_similar_dog
+import os
 
 app = Flask(__name__)
 
@@ -11,5 +12,9 @@ def index():
         result = get_most_similar_dog(img)
     return render_template('index.html', result=result)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
